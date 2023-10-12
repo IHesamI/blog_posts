e973c37de5014f097986071d5c48846463e60816
hesam <bhesam2000@yahoo.com>
2023-10-12 03:33:01 +0330

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './index.css'
+function App() {
+
+  return (
+    <>
+    <p
+    className="text-red-600 bg-gray-700">
+      This is a test
+    </p>
+    </>
+  )
+}
+
+export default App

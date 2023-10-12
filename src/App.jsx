977793fd5eefb7422ea0f220cb2a45dfdc08505b
hesam <bhesam2000@yahoo.com>
2023-10-12 03:36:29 +0330

    make an example of tailwind usage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import './index.css'
 function App() {
-
   return (
     <>
-    <p
-    className="text-red-600 bg-gray-700">
-      This is a test
-    </p>
+      <p className="text-red-600 bg-gray-700">This is a test</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

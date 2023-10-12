36549ab092504666e7eb13132f2fbed17e342d59
hesam <bhesam2000@yahoo.com>
2023-10-12 19:15:22 +0330

    add some functions for change themes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
+import { changeTheme } from "./utils/themeProvider";
+
 function App() {
+  useEffect(()=>{
+    const root = document.getElementById("root");
+    changeTheme(root);
+  },[]);
+
   return (
     <>
-      <p className="text-red-600 bg-gray-700">This is a test</p>
+      <p className="text-red-600 bg-background">This is a test</p>
     </>
   );
 }
